7e1f2fdb60eeb36734c76069ac68e9440602a055
Anjana Pradeep <32275066+anjanapradeep13@users.noreply.github.com>
2017-11-07 11:33:08 -0800

    Revert "Kayak FrontEnd"
    This reverts commit 8c40affa0f8147f72af81de20bcdc4869e504317.

--- KAYAK/front-end/src/App.js
+++ KAYAK/front-end/src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import HomePage from "./components/HomePage";
-
 
 class App extends Component {
-
-    constructor(){
-        super();
-
-        injectTapEventPlugin();
-    }
-
   render() {
     return (
-        <BrowserRouter >
-            <HomePage/>
-        </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
 
 export default App;
-
-
-
