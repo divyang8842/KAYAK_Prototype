79f5bb162c11bfcaa2ebb9e56c9714dfc97cb7f2
panchaltejas <tejas.panchal@sjsu.edu>
2017-11-25 00:20:50 -0800

    Implemented Hotel filters

--- KAYAK/front-end/src/components/Hotels/Results.js
+++ KAYAK/front-end/src/components/Hotels/Results.js
@@ -22,8 +22,14 @@ class Results extends Component {
     sort_price:0,
     filterparams: {
       price:"500",
-      star:"",
-      freebies:[]
+      star:"1",
+      freebies:[],
+      chk_breakfast: false,
+      chk_parking: false,
+      chk_fitness: false,
+      chk_pool: false,
+      chk_tennis: false,
+      chk_airport: false
     }
   }
 
@@ -47,11 +53,12 @@ class Results extends Component {
     });
   }
 
+
   createHotelsList(){
     return this.state.hotelsArray.map((hotelItem) => {
       var styles = {
         background:'white',
-        margin:'8px'
+        'margin-bottom':'8px'
       }
       return(
         <div className="col-md-8 col-sm-8 " style={styles}>
@@ -78,10 +85,6 @@ class Results extends Component {
       );
   });
   }
-
-  // filterHotels(){
-  //   this.props.loadFilteredHotels(this.props.hotels.hotels);
-  // }
 
   setSortCriteria(sort_criteria){
     if(sort_criteria == "price"){
@@ -137,9 +140,53 @@ class Results extends Component {
       }
       this.state.hotelsArray.sort(compareReviews);
     }
-
   }
 
+  filterHotels(){
+    var tempHotelsArray = [];
+    var price = this.state.filterparams.price;
+    var star = this.state.filterparams.star;
+    var freebies = this.state.filterparams.freebies;
+    this.props.hotels.hotels.map((hotelItem) => {
+      var amenities = hotelItem.hotel_amenities.split(',');
+      var filterflag = false;
+      if(hotelItem.standard_rates <= price){
+        if(hotelItem.hotel_star >= star){
+          for(var i=0; i<freebies.length; i++){
+            if(amenities.indexOf(freebies[i]) == -1){
+              filterflag = true;
+              break;
+            }
+          }
+          if(!filterflag){
+            tempHotelsArray.push(hotelItem);
+          }  
+        }
+      }
+    });
+    this.setState({
+      ...this.state,
+      hotelsArray: tempHotelsArray
+    });
+  }
+
+  setFreebies(){
+    var tempFreebies = [];
+    if(this.state.filterparams.chk_airport == true) tempFreebies.push("Airport Pick-up");
+    if(this.state.filterparams.chk_parking == true) tempFreebies.push("Free Parking");
+    if(this.state.filterparams.chk_fitness == true) tempFreebies.push("Fitness Center");
+    if(this.state.filterparams.chk_pool == true) tempFreebies.push("Swimming Pool");
+    if(this.state.filterparams.chk_tennis == true) tempFreebies.push("Tennis Court");
+    if(this.state.filterparams.chk_breakfast == true) tempFreebies.push("Free Breakfast");
+    this.setState({
+      filterparams:{
+        ...this.state.filterparams,
+        freebies: tempFreebies
+      }
+    },this.filterHotels);
+    console.log(this.state.filterparams.freebies);
+  }
+  
   render() {
     return (
       <div className="row">
@@ -148,22 +195,57 @@ class Results extends Component {
             <h4 id="h4filter">Star</h4>
             <hr />
             <span class="rating">
-              <input type="radio" class="rating-input"
-                    id="rating-input-1-5" name="rating-input-1"/>
+              <input type="radio" class="rating-input" value="1"
+                    id="rating-input-1-5" name="rating-input-1" onClick={(event) => {
+                      this.setState({
+                          ...this.state,
+                          filterparams: {
+                              ...this.state.filterparams,
+                              star: event.target.value
+                          }
+                      },this.filterHotels);}}/>
               <label for="rating-input-1-5" class="rating-star">
                 <p id="parstar">1</p>
               </label>
-              <input type="radio" class="rating-input"
-                    id="rating-input-1-4" name="rating-input-1"/>
+              <input type="radio" class="rating-input" value="2"
+                    id="rating-input-1-4" name="rating-input-1" onClick={(event) => {
+                      this.setState({
+                          ...this.state,
+                          filterparams: {
+                              ...this.state.filterparams,
+                              star: event.target.value
+                          }
+                      },this.filterHotels);}}/>
               <label for="rating-input-1-4" class="rating-star"><p id="parstar">2</p></label>
-              <input type="radio" class="rating-input"
-                    id="rating-input-1-3" name="rating-input-1"/>
+              <input type="radio" class="rating-input" value="3"
+                    id="rating-input-1-3" name="rating-input-1" onClick={(event) => {
+                      this.setState({
+                          ...this.state,
+                          filterparams: {
+                              ...this.state.filterparams,
+                              star: event.target.value
+                          }
+                      },this.filterHotels);}}/>
               <label for="rating-input-1-3" class="rating-star"><p id="parstar">3</p></label>
-              <input type="radio" class="rating-input"
-                    id="rating-input-1-2" name="rating-input-1"/>
+              <input type="radio" class="rating-input" value="4"
+                    id="rating-input-1-2" name="rating-input-1" onClick={(event) => {
+                      this.setState({
+                          ...this.state,
+                          filterparams: {
+                              ...this.state.filterparams,
+                              star: event.target.value
+                          }
+                      },this.filterHotels);}}/>
               <label for="rating-input-1-2" class="rating-star"><p id="parstar">4</p></label>
-              <input type="radio" class="rating-input"
-                    id="rating-input-1-1" name="rating-input-1"/>
+              <input type="radio" class="rating-input" value="5"
+                    id="rating-input-1-1" name="rating-input-1" onClick={(event) => {
+                      this.setState({
+                          ...this.state,
+                          filterparams: {
+                              ...this.state.filterparams,
+                              star: event.target.value
+                          }
+                      },this.filterHotels);}}/>
               <label for="rating-input-1-1" class="rating-star"><p id="parstar">5</p></label>
             </span>
             <br/>
@@ -185,23 +267,78 @@ class Results extends Component {
                         ...this.state.filterparams,
                         price: event.target.value
                     }
-                });}}
+                },this.filterHotels);}}
             />
             <br/>
             <br/>
+            
             <h4 id="h4filter">Freebies</h4>
             <hr/>
             <div class="control-group">
               <label class="control control--checkbox">Free Parking
-                <input type="checkbox"/>
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_parking: event.target.checked
+                    }
+                },this.setFreebies);}}/>
                 <div class="control__indicator"></div>
               </label>
               <label class="control control--checkbox">Fitness Center
-                <input type="checkbox"/>
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_fitness: event.target.checked
+                    }
+                },this.setFreebies);}}/>
                 <div class="control__indicator"></div>
               </label>
               <label class="control control--checkbox">Free Breakfast
-                <input type="checkbox"/>
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_breakfast: event.target.checked
+                    }
+                },this.setFreebies);}}/>
+                <div class="control__indicator"></div>
+              </label>
+              <label class="control control--checkbox">Swimming Pool
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_pool: event.target.checked
+                    }
+                },this.setFreebies);}}/>
+                <div class="control__indicator"></div>
+              </label>
+              <label class="control control--checkbox">Tennis Court
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_tennis: event.target.checked
+                    }
+                },this.setFreebies);}}/>
+                <div class="control__indicator"></div>
+              </label>
+              <label class="control control--checkbox">Airport Pick-up
+                <input type="checkbox" onChange={(event) => {
+                this.setState({
+                    ...this.state,
+                    filterparams: {
+                        ...this.state.filterparams,
+                        chk_airport: event.target.checked
+                    }
+                },this.setFreebies);}}/>
                 <div class="control__indicator"></div>
               </label>
             </div>
