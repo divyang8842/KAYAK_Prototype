9bdcd9dc736f1248584e3d89b61d52f3a34a2ad1
DESKTOP-RALGLS1\janhu <jay.desai@sjsu.edu>
2017-12-01 01:26:20 -0800

    UserTracking Updated

--- KAYAK/kafka_back_end/services/UserTracking/Charts.js
+++ KAYAK/kafka_back_end/services/UserTracking/Charts.js
@@ -10,10 +10,20 @@ var ObjectID = require('mongodb').ObjectID;
 function handle_Request(msg, callback) {
 
     var response =[];
+    mongo.connect(mongoURL, function () {
 
-    response.code = "200";
-    console.log("Success--- inside Tracking User- HOTEL_PAGE"+response);
-    callback(null, response);
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('usertrackingcharts');
+
+        coll.findOne({"userid": msg.userid}, function (err, searchuser) {
+            if (searchuser) {
+                response.code = "200";
+                console.log("Success--- inside Tracking User- HOTEL_PAGE"+response);
+                callback(null, response);
+            }}
+            );
+    });
+
 
 }
 
