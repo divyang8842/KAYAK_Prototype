994844cd2d6d890fc9ca8985c49b0894b10906c7
DESKTOP-RALGLS1\janhu <jay.desai@sjsu.edu>
2017-12-03 17:39:48 -0800

    UserTracking page is updated

--- KAYAK/Back_End/routes/UserTracking/UserTracking.js
+++ KAYAK/Back_End/routes/UserTracking/UserTracking.js
@@ -3,14 +3,18 @@ var kafka = require('../kafka/client');
 
 exports.usertracking= function(req,res) {
     var userid;
+    var city;
     if(req.session.user)
     {
         userid = req.session.user.id;
+        city=req.session.user.city;
     }
     else
     {
         userid = "";
     }
+
+
     var sessionid = req.sessionID;
 
     console.log("Inside User Tracking");
@@ -20,7 +24,8 @@ exports.usertracking= function(req,res) {
             "previous_page":req.body.previous_page,
             "user_id":userid,
             "session_id":sessionid,
-            "timeonpage":req.body.timeonpage
+            "timeonpage":req.body.timeonpage,
+            "city":city
         },
         function(err,results){
             console.log('in result');
